7f87c01ed50aeeb9d9c40c496ccf517b9f375d5b
Andrew Hammett <am.hammett@gmail.com>
2014-08-06 20:49:55 +1000

    ahammett: foundation for vpn service

---
+++ go-vpn-service-stop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ "$#" -lt 3 ]] ; then
+  echo "usage: $0 <HOST> <USER> <KEY_FILE>"
+  exit 1
+fi
+
+HOST=$1
+USER=$2
+KEY_FILE=$3
+
+ansible-playbook vpn-service.yml -i $HOST, \
+  --user=$USER --private-key=$KEY_FILE \
+  --extra-vars "state=stop"
