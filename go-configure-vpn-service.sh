0934d44c8879ca904a0ef65bad12563436263dab
Andrew Hammett <am.hammett@gmail.com>
2014-08-08 12:25:19 +1000

    ahammett: pass through vpn details

--- go-configure-vpn-service.sh
+++ go-configure-vpn-service.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-if [[ "$#" -lt 3 ]] ; then
-  echo "usage: $0 <HOST> <USER> <KEY_FILE>"
+if [[ "$#" -lt 6 ]] ; then
+  echo "usage: $0 <HOST> <USER> <KEY_FILE> <VPN_USER> <VPN_PASSWORD> <VPN_SERVER>"
   exit 1
 fi
 
 HOST=$1
 USER=$2
 KEY_FILE=$3
+VPN_USER=$4
+VPN_PASSWORD=$5
+VPN_SERVER=$6
 
 ansible-playbook configure-vpn-service.yml -i $HOST, \
-  --user=$USER --private-key=$KEY_FILE
+  --user=$USER --private-key=$KEY_FILE \
+  --extra-vars="vpn_user=$VPN_USER vpn_pass=$VPN_PASSWORD vpn_server=$VPN_SERVER"
