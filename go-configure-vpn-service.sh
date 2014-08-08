0da74abcb0bb27e6cdd8f32a7f8858f7b5ffa592
Andrew Hammett <am.hammett@gmail.com>
2014-08-08 14:49:20 +1000

    ahammett: now requires vpn ip and psk

--- go-configure-vpn-service.sh
+++ go-configure-vpn-service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ "$#" -lt 6 ]] ; then
-  echo "usage: $0 <HOST> <USER> <KEY_FILE> <VPN_USER> <VPN_PASSWORD> <VPN_SERVER>"
+if [[ "$#" -lt 7 ]] ; then
+  echo "usage: $0 <HOST> <USER> <KEY_FILE> <VPN_USER> <VPN_PASSWORD> <VPN_IP> <VPN_SECRET>"
   exit 1
 fi
 
@@ -10,8 +10,9 @@ USER=$2
 KEY_FILE=$3
 VPN_USER=$4
 VPN_PASSWORD=$5
-VPN_SERVER=$6
+VPN_IP=$6
+VPN_SECRET=$6
 
 ansible-playbook configure-vpn-service.yml -i $HOST, \
   --user=$USER --private-key=$KEY_FILE \
-  --extra-vars="vpn_user=$VPN_USER vpn_pass=$VPN_PASSWORD vpn_server=$VPN_SERVER"
+  --extra-vars="vpn_user=$VPN_USER vpn_pass=$VPN_PASSWORD vpn_ip=$VPN_IP vpn_psk=$VPN_SECRET"
