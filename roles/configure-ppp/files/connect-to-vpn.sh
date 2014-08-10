8391c1e8913e1ce41c5473315844096472eb79f1
Andrew Hammett <am.hammett@gmail.com>
2014-08-10 21:52:16 +1000

    ahammett: connect to vpn script to run with cron

---
+++ roles/configure-ppp/files/connect-to-vpn.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+location=$(curl -s strongvpn.com | grep images/flags | cut -d '"' -f2 | cut -d '/' -f3 | cut -d '.' -f1)
+log="/tmp/vpn.log"
+
+function logger () {
+  log_time=$(date +"%d %b %Y %H:%M")
+
+  echo "$log_time: $1" >> $log
+}
+
+
+if [[ $location != 'us' ]]; then
+  logger "not connected to vpn."
+  logger "expected us, found:  ${location}"
+  sudo poff -a
+  sleep 5
+
+  logger "connecting to vpn"
+  sudo pon vpn-connection
+fi
+
+logger "vpn connected"
